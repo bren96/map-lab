941e5647b5390d493d4c1a779b3e8364b074f67e
bren96 <brendancullen996@gmail.com>
2023-09-07 20:20:49 -0400

    wip

--- libs/ui-kit/src/lib/avatar/avatar.tsx
+++ libs/ui-kit/src/lib/avatar/avatar.tsx
@@ -1,14 +1,115 @@
-import styles from './avatar.module.scss';
+import { ComponentProps } from "react";
+import { getInitials } from "../utils/getInitials";
+import { Tooltip } from "../tooltip";
+import styles from "./avatar.module.scss";
+import { clsx } from "clsx";
 
-/* eslint-disable-next-line */
-export interface AvatarProps {}
 
-export function Avatar(props: AvatarProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to Avatar!</h1>
+const DEFAULT_SIZE = 24;
+const DEFAULT_NAME = '...';
+const DEFAULT_COLOR = '#000';
+const FONT_SIZE_FACTOR = 0.36;
+
+export interface Props extends Omit<ComponentProps<"div">, "color"> {
+  src?: string;
+  name: string;
+  size?: number;
+  color?: string;
+  outline?: boolean;
+  tooltip?: boolean;
+  tooltipProps?: Omit<ComponentProps<typeof Tooltip>, "children" | "content">;
+}
+
+interface EllipsisProps extends ComponentProps<"div"> {
+  ellipsis: number;
+  size?: number;
+  outline?: boolean;
+  tooltip?: boolean;
+  tooltipProps?: Omit<ComponentProps<typeof Tooltip>, "children" | "content">;
+}
+
+export function Avatar({
+  src,
+  size = DEFAULT_SIZE,
+  name = DEFAULT_NAME,
+  color = DEFAULT_COLOR,
+  outline = false,
+  tooltip = false,
+  tooltipProps,
+  className,
+  style,
+  ...props
+}: Props) {
+  const content = (
+    <div
+      className={clsx(
+        styles.avatar,
+        className,
+        outline && styles.avatarOutline
+      )}
+      style={{ width: size, height: size, color, ...style }}
+      aria-label={name}
+      {...props}
+    >
+      {src && (
+        <Image alt={name} src={src} height={size} width={size} aria-hidden />
+      )}
+      <span
+        style={{ fontSize: size * FONT_SIZE_FACTOR }}
+        className={styles.label}
+        aria-hidden
+      >
+        {getInitials(name)}
+      </span>
     </div>
+  );
+
+  return tooltip ? (
+    <Tooltip content={name} {...tooltipProps}>
+      {content}
+    </Tooltip>
+  ) : (
+    content
   );
 }
 
-export default Avatar;
+export function AvatarEllipsis({
+  ellipsis,
+  size = DEFAULT_SIZE,
+  outline = false,
+  tooltip = false,
+  tooltipProps,
+  className,
+  style,
+  ...props
+}: EllipsisProps) {
+  const content = (
+    <div
+      className={clsx(
+        styles.avatar,
+        className,
+        outline && styles.avatarOutline
+      )}
+      style={{ width: size, height: size, ...style }}
+      {...props}
+    >
+      <span
+        style={{ fontSize: size * FONT_SIZE_FACTOR }}
+        className={styles.label}
+      >
+        +{ellipsis}
+      </span>
+    </div>
+  );
+
+  return tooltip ? (
+    <Tooltip
+      content={`${ellipsis} other${ellipsis > 1 ? "s" : ""}`}
+      {...tooltipProps}
+    >
+      {content}
+    </Tooltip>
+  ) : (
+    content
+  );
+}
--- makefile
+++ makefile
@@ -2,10 +2,21 @@ build-all:
 	docker compose build monorepo
 	docker compose build ui-kit
 
-bash-monorepo:
-	docker compose run --build monorepo bash
-
 cleanup-docker:
 	docker container prune -f
 	docker image prune -f
 	docker network prune -f
+
+# Monorepo Commands
+bash-monorepo:
+	docker compose run --build monorepo bash
+
+build-monorepo:
+	docker compose build monorepo
+
+# Ui-Kit Commands
+up-ui-kit:
+	docker compose up --build ui-kit
+
+build-ui-kit:
+	docker compose build ui-kit
