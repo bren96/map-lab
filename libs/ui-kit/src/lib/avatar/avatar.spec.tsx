5ea819a1b6831f971f6695a630eaf003647afdef
bren96 <brendancullen996@gmail.com>
2023-09-09 04:15:09 +0000

    fix tests

--- libs/ui-kit/src/lib/avatar/avatar.spec.tsx
+++ libs/ui-kit/src/lib/avatar/avatar.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Avatar from './avatar';
+import { Avatar } from './avatar';
 
 describe('Avatar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Avatar />);
+    const { baseElement } = render(<Avatar name='Brendawg666'/>);
     expect(baseElement).toBeTruthy();
   });
 });
