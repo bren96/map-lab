681a6203de6b07f5c8eea6070dea70df6634f171
Brendan Cullen <brendancullen996@gmail.com>
2024-02-16 15:39:33 -0500

    follow tailwind setup instructions from nextjs

--- apps/map-lab-next/app/page.tsx
+++ apps/map-lab-next/app/page.tsx
@@ -1,11 +1,9 @@
-import styles from './page.module.scss';
-
 /* eslint-disable-next-line */
 export interface PageProps {}
 
 export default function Page(props: PageProps) {
   return (
-    <div className={styles['container']}>
+    <div className="text-indigo-600">
       <h1>Welcome to Page!</h1>
     </div>
   );
