2bea62d5c9aea9a1f5a26105858c7d200565ecec
Brendan Cullen <brendancullen996@gmail.com>
2024-02-16 12:04:56 -0500

    default single function in Page.tsx

--- apps/map-lab-next/app/page.tsx
+++ apps/map-lab-next/app/page.tsx
@@ -3,12 +3,10 @@ import styles from './page.module.scss';
 /* eslint-disable-next-line */
 export interface PageProps {}
 
-export function Page(props: PageProps) {
+export default function Page(props: PageProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Page!</h1>
     </div>
   );
 }
-
-export default Page;
