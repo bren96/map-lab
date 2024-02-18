ef79e24c9b33adcea1bddda119013ccd119632a2
Brendan Cullen <brendancullen996@gmail.com>
2024-02-17 21:33:50 -0500

    rename tailwind examples

---
+++ libs/ui-kit/src/lib/tailwind-examples.tsx
@@ -0,0 +1,9 @@
+export function TailwindExamples() {
+  return (
+    <>
+      <p className="text-1xl">text-1xl</p>
+      <p className="text-2xl">text-2xl</p>
+      <p className="text-3xl">text-3xl</p>
+    </>
+  );
+}
